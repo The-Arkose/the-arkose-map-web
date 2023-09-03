c6452079b7b1df18cea89257ef77e48683572c7c
marklai1998 <marklai1998@gmail.com>
2023-09-03 14:58:30 -0400

    chore: fix type

--- src/containers/Map/index.tsx
+++ src/containers/Map/index.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled'
 import { useParams } from '@tanstack/react-router'
 
 export const Map: React.FC = () => {
-  const { mapId } = useParams()
+  const { mapId } = useParams({ strict: false })
 
   return <Iframe src={`https://dynmap.thearkose.com/${mapId}/`} />
 }
